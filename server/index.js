a4ec1dd8fde0b3b542d6deef03313d304b95df2a
Mohamed Albasuony <97564022+mohamedalbasuony1@users.noreply.github.com>
2024-05-11 17:05:01 -0400

    Server is runining with no issues

--- server/index.js
+++ server/index.js
@@ -4,12 +4,27 @@ const app = express()
 
 const port = 3000 
 
+app.use(express.static('public'))
 
+
+
+app.get('/', (req, res)=>{
+    res.send('Server is runining well')
+    
+    })
 app.get('/message', (req, res)=>{
 res.send('hello from our server')
 
 })
 
+
+
+app.post('/api', (req, res)=>{
+    res.send("received post request")
+})
+
 app.listen(port, ()=>{
     console.log('Server is operational')
 })
+
+
